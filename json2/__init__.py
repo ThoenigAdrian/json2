e57c20a8d46ea1a4cef82743cfa68df41e9874ad
ThoenigAdrian <thoenig.adrian@hotmail.com>
2020-10-14 16:51:36 +0200

    Update __init__.py

--- json2/__init__.py
+++ json2/__init__.py
@@ -1,10 +1,10 @@
 from json import *
 
-def read_file(filename):
+def load_file(filename):
     with open(filename, "r") as f:
         return json.loads(f.read())
 
 
-def write_file(filename, content):
+def dump_file(filename, content):
     with open(filename, "w") as f:
         f.write(json.dumps(content))
