60d6e9b95b7176dde0555776c38546399414fcda
Adrian Thoenig <thoenig.adrian@hotmail.com>
2023-10-10 12:14:28 +0200

    MLFriendlyEncoder added

--- json2/__init__.py
+++ json2/__init__.py
@@ -8,3 +8,17 @@ def read_file(filename):
 def write_file(filename, content):
     with open(filename, "w") as f:
         f.write(json.dumps(content))
+
+
+class MLFrienldyEncoder(json.JSONEncoder):
+    """
+        A custom JSON encoder that can handle NumPy arrays and PyTorch tensors.
+    """
+    def default(self, obj):
+        import torch
+        import numpy as np
+        if isinstance(obj, (np.ndarray, torch.Tensor)):
+            if isinstance(obj, torch.Tensor):
+                obj = obj.detach().cpu().numpy()  # Convert PyTorch tensor to NumPy array
+            return obj.tolist()  # Convert NumPy array to Python list
+        return super(MLFrienldyEncoder, self).default(obj)
